5008ba0e53e98b13fd2146eedfa46189e7842270
4reha <ael-hadd@student.1337.ma>
2022-11-09 15:23:42 +0100

    update main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include "Server.hpp"
-#include "Clients.hpp"
-
+#include "Client.hpp"
 
 void error_lol(std::string msg)
 {
-    std::cerr << msg << std::endl;
-    exit(1);
+	std::cerr << msg << std::endl;
+	exit(1);
 }
 
-bool	isAllNum(std::string str)
+bool isAllNum(std::string str)
 {
 	for (size_t i = 0; i < str.size(); i++)
-		if (! std::isdigit(str[i]))
+		if (!std::isdigit(str[i]))
 			return (false);
 	return (true);
 }
 
-int main(int ac, char** av)
+int main(int ac, char **av)
 {
-    if (ac != 3)
-        error_lol("Usage error: ./ircserv <port> <password>");
+	if (ac != 3)
+		error_lol("Usage error: ./ircserv <port> <password>");
 
 	if (!isAllNum(av[1]))
 		error_lol("Error: Port: Not A Number!");

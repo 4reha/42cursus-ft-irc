7c63447c23b6b9ef8fa0a76d93735f304abe5206
4reha <ael-hadd@student.1337.ma>
2022-11-10 11:28:56 +0100

    adding methods to the class

--- Channel.hpp
+++ Channel.hpp
@@ -14,23 +14,47 @@
 
 #include <map>
 #include "Client.hpp"
-#include "Server.hpp"
+// #include "Server.hpp"
 
+class Client;
 class Channel
 {
-	private:
-		/* data */
-	public:
+private:
+	/* data */
+public:
+	size_t						maxMembers;
+	std::string 				Name;
+	std::string 				Password;
+	std::vector<Client *> 		Members;
+	std::vector<Client *> 		Operators;
+	std::vector<Client *> 		Banned;
 
-		int									maxMembers;
-		std::string 						Name;
-		std::map<std::string, Client*> 		Members;
-		std::map<std::string, Client*> 		Operators;
-		std::map<std::string, Client*> 		Banned;
-		
-		Channel(std::string name);
-		~Channel();
+	Channel(std::string name);
+	~Channel();
+
+	void broadcast_msg(std::string msg);
+	bool isBanned(std::string user);
+	bool isFull();
 };
+
+void	Channel::broadcast_msg(std::string msg)
+{
+	for (size_t i = 0; i < Members.size(); i++)
+		Members[i]->pending_msgs.push_back(msg);
+}
+
+bool Channel::isBanned(std::string user)
+{
+	for (size_t i = 0; i < this->Banned.size(); i++)
+		if (user == Banned[i]->nickname)
+			return (true);
+	return (false);
+}
+
+bool Channel::isFull()
+{
+	return (this->Members.size() == this->maxMembers);
+}
 
 Channel::Channel(std::string name)
 {
